374d9e0305e56887ceb4d862b77f983379697865
FADILI EL MOSTAFA <elmostafa.fadili@enova.ma>
2023-04-15 13:50:30 +0000

    Initial commit

---
+++ src/test/java/fadili/mostafa/springsecuritybasic/SpringSecurityBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package fadili.mostafa.springsecuritybasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
